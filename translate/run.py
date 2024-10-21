20af22e263e67ab8cfd572cc7d0e2afaa07c61b8
H1rono <hronok66@gmail.com>
2024-10-21 11:14:01 +0900

    Add `translate` working directory

---
+++ translate/run.py
@@ -0,0 +1,26 @@
+import sys
+from os import fdopen
+
+from bf.tokenize import Tokenize
+from bf.parse import Parser
+from bf.run import run
+
+
+def entry_point(argv):
+    """
+    entry_point(argv: list[str]) -> int
+    """
+    filename = argv[1]
+    p = Parser()
+    with open(filename) as f, fdopen(0) as stdin, fdopen(1, "w") as stdout:
+        program = p.parse(Tokenize(f))
+        run(program, stdin, stdout)
+    return 0
+
+
+def target(*args):
+    return entry_point, None
+
+
+if __name__ == "__main__":
+    entry_point(sys.argv)
