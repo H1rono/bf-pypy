d165752bd51617ebae34f8b36d127a658a82d329
H1rono <hronok66@gmail.com>
2024-10-26 17:16:43 +0900

    Generate `.pth` file, create symlink in `USER_SITE`

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,5 +4,9 @@ set -eu -o pipefail
 
 USER_SITE="$(pypy -c 'import site; print site.USER_SITE')"
 mkdir -p "$USER_SITE"
-cp pypy.pth "${USER_SITE}/pypy.pth"
+cat - << EOF > "$USER_SITE/pypy.pth"
+$PWD/pypy
+$PWD
+EOF
+ln -s "$PWD/pypy/rpython" "$USER_SITE/rpython"
 pip install -e '.[dev]'
