316b1036fe6ed62e077607285086f20d26f7a4ec
H1rono <hronok66@gmail.com>
2024-11-10 15:11:48 +0900

    :sparkles: Use `run.py` API in `runjit.py`

--- bf/runjit.py
+++ bf/runjit.py
@@ -3,91 +3,35 @@ from argparse import ArgumentParser
 
 from rpython.rlib.jit import JitDriver
 
-from . import tokenize
+from . import tokenize, run as bf_run
 from .machine import Machine
 from .parse import Parser
 from .program import Program
 from .token import is_token
 
 
-def _get_location(i, is_loop, p, program):
-    """
-    _get_location(i: int, is_loop: bool, p: Program, program: list[Program], machine: Machine) -> str
-    """
-    # assert isinstance(i, int)
-    # assert isinstance(is_loop, bool)
-    # assert isinstance(p, Program)
-    # assert isinstance(program, list)
-    # assert isinstance(machine, Machine)
-    prev = program[:i]
-    prev_str = ""
-    for pr in prev:
-        prev_str += pr.raw_str()
-    rest = program[i + 1:0]
-    rest_str = ""
-    for re in rest:
-        rest_str += re.raw_str()
-    return "%s_%s_%s" % (
-        prev_str,
-        p.raw_str(),
-        rest_str,
-    )
-
-
-def run_token(machine, token):
-    """
-    run_token(machine: Machine, token: Program.token) -> None
-    """
-    from .token import INCREMENT, DECREMENT, ADVANCE, DEVANCE, WRITE, READ, LOOP_BEGIN, LOOP_END
-
-    # assert isinstance(machine, Machine)
-    # assert isinstance(token, Program) and token.kind == Program.KIND_TOKEN
-    token_inner = token.token
-    # assert token_inner is not None and is_token(token_inner)
-    # assert token_inner not in [LOOP_BEGIN, LOOP_END]
-    v = machine.tape.value()
-    if token_inner == INCREMENT:
-        machine.tape.set_value(v + 1)
-    elif token_inner == DECREMENT:
-        machine.tape.set_value(v - 1)
-    elif token_inner == ADVANCE:
-        machine.tape.advance_by(1)
-    elif token_inner == DEVANCE:
-        machine.tape.devance_by(1)
-    elif token_inner == WRITE:
-        machine.write()
-    elif token_inner == READ:
-        machine.read()
-
-
 _jit_driver = JitDriver(
-    greens=["i", "is_loop", "p", "program"],
+    greens=["i", "program", "token"],
     reds=["machine"],
-    get_printable_location=_get_location,
-    is_recursive=True,
 )
 
 
-def run_inner(program, machine, is_loop=False):
+def _iteration(machine, program, i, token):
+    """
+    _iteration(machine: Machine, program: list[Program], i: int, token: Program.token) -> None
+    """
+    _jit_driver.jit_merge_point(i=i, program=program, token=token, machine=machine)
+    bf_run.run_token(machine, token)
+
+
+def run_inner(program, machine):
     """
     run_inner(program: list[Program], machine: Machine, is_loop: bool) -> None
     """
-    i = 0
-    while i < len(program):
-        if is_loop and i == 0 and machine.tape.value() == 0:
-            return
-        p = program[i]
-        _jit_driver.jit_merge_point(i=i, is_loop=is_loop, p=p, program=program, machine=machine)
-        if p.kind == Program.KIND_TOKEN:
-            # assert is_token(p.token)
-            run_token(machine, p)
-        else:
-            # assert p.kind == Program.KIND_LOOP and p.loop is not None
-            run_inner(p.loop, machine, is_loop=True)
-        i += 1
-        if is_loop:
-            i %= len(program)
-        # _jit_driver.can_enter_jit(i=i, is_loop=is_loop, p=p, program=program, machine=machine)
+    ctx = bf_run.Context(machine, program)
+    for c in ctx:
+        machine, program, i, token = c
+        _iteration(machine, program, i, token)
 
 
 def run(program, stdin, stdout):
@@ -95,7 +39,7 @@ def run(program, stdin, stdout):
     run(program: list[Program], stdin: File, stdout: File) -> None
     """
     machine = Machine(stdin, stdout)
-    run_inner(program, machine, is_loop=False)
+    run_inner(program, machine)
 
 
 def set_args(parser):
