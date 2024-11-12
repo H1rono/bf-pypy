ea3d3bc42dc77773d974520fa41f30bcacf28021
H1rono <hronok66@gmail.com>
2024-11-12 11:19:02 +0900

    Adjust `runjit`

--- bf/runjit.py
+++ bf/runjit.py
@@ -3,46 +3,34 @@ from argparse import ArgumentParser
 
 from rpython.rlib.jit import JitDriver
 
-from . import tokenize, run as bf_run
+from . import tokenize
 from .machine import Machine
-from .parse import Parser
+from .parse import parse
+from .run import run_token, Frames
 
 
 _jit_driver = JitDriver(
-    greens=["i", "program", "token"],
+    greens=["pos", "program", "token"],
     reds=["machine"],
     is_recursive=True
 )
 
 
-def _iteration(machine, program, i, token):
-    """
-    _iteration(machine: Machine, program: list[Program], i: int, token: Program.token) -> None
-    """
-    from .token import LOOP_BEGIN, LOOP_END
-
-    _jit_driver.jit_merge_point(i=i, program=program, token=token, machine=machine)
-    # assert token.kind = Program.KIND_TOKEN and is_token(token.token)
-    bf_run.run_token(machine, token.token)
-    _jit_driver.can_enter_jit(i=i, program=program, token=token, machine=machine)
-
-
-def run_inner(program, machine):
-    """
-    run_inner(program: list[Program], machine: Machine, is_loop: bool) -> None
-    """
-    ctx = bf_run.Context(machine, program)
-    for c in ctx:
-        machine, program, i, token = c
-        _iteration(machine, program, i, token)
+def _iteration(frame):
+    pos, machine, program = frame
+    token = program.tokens[pos.at]
+    _jit_driver.jit_merge_point(pos=pos, program=program, token=token, machine=machine)
+    run_token(pos, machine, token, program)
 
 
 def run(program, stdin, stdout):
     """
-    run(program: list[Program], stdin: File, stdout: File) -> None
+    run(program: ParseResult, stdin: File, stdout: File) -> None
     """
     machine = Machine(stdin, stdout)
-    run_inner(program, machine)
+    frames = Frames(machine, program)
+    for f in frames:
+        _iteration(f)
 
 
 def set_args(parser):
@@ -57,9 +45,8 @@ def main():
     parser = ArgumentParser("parse")
     tokenize.set_args(parser)
     args = parser.parse_args()
-    parser = Parser()
     with tokenize.acquire_from_args(args) as t:
-        program = parser.parse(t).collect()
+        program = parse(t)
     run(program, sys.stdin, sys.stdout)
 
 
