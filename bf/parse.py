81811ca3091f0120cf9543c62e0538774a09affe
H1rono <hronok66@gmail.com>
2024-11-14 02:19:48 +0900

    Split `parse_bracket_map`

--- bf/parse.py
+++ bf/parse.py
@@ -30,12 +30,31 @@ def parse_simple(tokens):
     return ("".join(raw), instr)
 
 
-def parse(tokens):
-    parsed = []
+def parse_bracket_map(program, instructions):
     bracket_map = {}
     leftstack = []
-    simple = []
+    pc = 0
+    for instr in instructions:
+        kind, _vds, _dpos, rng = instr
+        begin, _end = rng
+        code = program[begin]
+        if kind != instruction.KIND_ONE_CHAR or code not in BRACKET:
+            pc += 1
+            continue
+        if code == LOOP_BEGIN:
+            leftstack.append(pc)
+        else: # code == LOOP_END
+            left = leftstack.pop()
+            right = pc
+            bracket_map[left] = right
+            bracket_map[right] = left
+        pc += 1
+    return bracket_map
 
+
+def parse(tokens):
+    parsed = []
+    simple = []
     for pc, char in tokens.enumerate():
         # assert char in MEMBERS
         if char in SIMPLES:
@@ -46,16 +65,9 @@ def parse(tokens):
             simple = []
             parsed.append((raw, instr))
         instr = instruction.one_char(pc)
-        if char == LOOP_BEGIN:
-            leftstack.append(len(parsed))
-        elif char == LOOP_END:
-            left = leftstack.pop()
-            right = len(parsed)
-            bracket_map[left] = right
-            bracket_map[right] = left
         parsed.append((char, instr))
-
-    raw = [r for r, _ in parsed]
+    raw = "".join([r for r, _ in parsed])
     instructions = [instr for _, instr in parsed]
+    bracket_map = parse_bracket_map(raw, instructions)
     metadata = (instructions, bracket_map)
-    return ("".join(raw), metadata)
+    return (raw, metadata)
