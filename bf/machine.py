05c4553352fdd4c29a731766be61053c8cadc66a
H1rono <hronok66@gmail.com>
2024-11-14 16:41:54 +0900

    Move `Machine` methods to `Tape`

--- bf/machine.py
+++ bf/machine.py
@@ -9,20 +9,6 @@ class Machine(Tape):
         self.stdin = stdin.fileno()
         self.stdout = stdout.fileno()
 
-    def accept_val_diffs(self, val_diffs):
-        for dpos, dval in val_diffs:
-            pos = self.position + dpos
-            ext_len = pos - len(self.thetape)
-            if ext_len >= 0:
-                self.thetape.extend([0] * (ext_len + 1))
-            self.thetape[pos] += dval
-
-    def mul_accept_val_diffs(self, val_diffs):
-        mul_by = self.get()
-        mul_val_diffs = [(dpos, dval * mul_by) for dpos, dval in val_diffs]
-        self.accept_val_diffs(mul_val_diffs)
-        self.set(0)
-
     def read(self):
         buf = read(self.stdin, 1)[0]
         self.set(ord(buf))
--- bf/tape.py
+++ bf/tape.py
@@ -24,3 +24,17 @@ class Tape(object):
 
     def devance_by(self, diff):
         self.position -= diff
+
+    def accept_val_diffs(self, val_diffs):
+        for dpos, dval in val_diffs:
+            pos = self.position + dpos
+            ext_len = pos - len(self.thetape)
+            if ext_len >= 0:
+                self.thetape.extend([0] * (ext_len + 1))
+            self.thetape[pos] += dval
+
+    def mul_accept_val_diffs(self, val_diffs):
+        mul_by = self.get()
+        mul_val_diffs = [(dpos, dval * mul_by) for dpos, dval in val_diffs]
+        self.accept_val_diffs(mul_val_diffs)
+        self.set(0)
