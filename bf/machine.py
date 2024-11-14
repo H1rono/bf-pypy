aa54dad157c2bb56e1a0cb0c97ec9d0d8600e8f5
H1rono <hronok66@gmail.com>
2024-11-14 18:10:56 +0900

    Avoid using `fileno`

--- bf/machine.py
+++ bf/machine.py
@@ -6,13 +6,15 @@ from .tape import Tape
 class Machine(Tape):
     def __init__(self, stdin, stdout):
         Tape.__init__(self)
-        self.stdin = stdin.fileno()
-        self.stdout = stdout.fileno()
+        self.stdin = stdin
+        self.stdout = stdout
 
     def read(self):
-        buf = read(self.stdin, 1)[0]
+        # buf = read(self.stdin, 1)[0]
+        buf = self.stdin.read(1)[0]
         self.set(ord(buf))
 
     def write(self):
         buf = chr(self.get())
-        write(self.stdout, buf)
+        # write(self.stdout, buf)
+        self.stdout.write(buf)
